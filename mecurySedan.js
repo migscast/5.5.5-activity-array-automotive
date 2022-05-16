a2c76ad8c73c5b2eab3b6abf0e70f2f4cf1fe92b
Miguel C <migs.castaneda@gmail.com>
2022-05-16 17:36:47 -0400

    first commit

--- mecurySedan.js
+++ mecurySedan.js
@@ -4,3 +4,46 @@ const VehicleModule = require("./vehicleBaseClass")
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
+class Car extends Vehicle{
+    constructor(make, model, year, color, mileage) {
+        super(make, model, year, color, mileage)
+        this.maxPassengers = maxPassengers;
+        this.passenger = passenger;
+        this.numberOfWheels = numberOfWheels;
+        this.maxSpeed = maxSpeed;
+        this.fuel = fuel;
+        this.scheduleService = scheduleService 
+    }
+
+    checkService() {
+        if (this.mileage > 30000) {            
+            this.scheduleService = true
+            return this.scheduleService;                       
+        }
+    }
+
+    start() {
+        if (this.fuel > 0) {            
+            console.log("engine has started.");
+            return this.started = true
+        } else {
+            console.log("no fuel");
+            return this.started = false;
+        }
+    }
+
+    loadPassenger(num) {
+        if (this.passenger < this.maxPassengers) {
+            if ((num + this.passenger) <= this.maxPassengers) {
+                this.passenger = num;
+                return this.passenger;               
+            } else {
+                console.log(this.model + " " + this.make + " not have enough space to take all passengers.");
+
+            }
+        } else {
+            console.log(this.model + " " + this.make + " is full");
+        }
+    }
+
+}
